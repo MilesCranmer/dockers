8f318bb5b085638536bfe82fe6ef2b3062353e0d
MilesCranmer <miles.cranmer@gmail.com>
2017-06-01 21:12:31 +1000

    Leave user for tty so can use vim

--- docker_cmd_tty.sh
+++ docker_cmd_tty.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/workspace mcranmer/dockers:$@
+docker run --rm -it --net=none -v "$PWD":/workspace mcranmer/dockers:$@
