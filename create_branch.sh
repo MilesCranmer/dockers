ec2635ca0ab5d1c81f55a10aa0c2a2ebb9fdbc05
MilesCranmer <miles.cranmer@gmail.com>
2017-06-01 06:50:08 +0000

    Remove confirm and add &&

--- create_branch.sh
+++ create_branch.sh
@@ -1,7 +1,7 @@
-git checkout --orphan $1
-ls
-confirm git reset --hard
-git checkout master/$1 ./Dockerfile
-git add Dockerfile
-git commit -m "Created branch for $1"
+git checkout --orphan $1 &&
+ls  &&
+git reset --hard  &&
+git checkout master/$1 ./Dockerfile  &&
+git add Dockerfile  &&
+git commit -m "Created branch for $1"  &&
 git push --set-upstream origin $1
