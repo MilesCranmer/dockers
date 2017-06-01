6a36882337e77c52c5bca3ab37805e1b303ae755
MilesCranmer <miles.cranmer@gmail.com>
2017-06-01 06:55:57 +0000

    Create branch now does a git mv

--- create_branch.sh
+++ create_branch.sh
@@ -2,7 +2,7 @@ git checkout --orphan $1 &&
 ls  &&
 git reset --hard  &&
 git checkout master $1 &&
-mv $1 Dockerfile &&
+git mv $1 Dockerfile &&
 git add Dockerfile  &&
 git commit -m "Created branch for $1"  &&
 git push --set-upstream origin $1
