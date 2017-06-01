2f737174647dcf830e80513a1a81fbfea93a37c5
MilesCranmer <miles.cranmer@gmail.com>
2017-06-01 07:10:42 +0000

    Set up new directory structure

--- .repo_scripts/create_branch.sh
+++ .repo_scripts/create_branch.sh
@@ -1,8 +1,8 @@
 git checkout --orphan $1 &&
 ls  &&
 git reset --hard  &&
-git checkout master $1 &&
-git mv $1 Dockerfile &&
+git checkout master dockerfiles/$1 &&
+git mv dockerfiles/$1 Dockerfile &&
 git add Dockerfile  &&
 git commit -m "Created branch for $1"  &&
 git push --set-upstream origin $1
--- .repo_scripts/update_branch.sh
+++ .repo_scripts/update_branch.sh
@@ -1,6 +1,6 @@
 git checkout $1 &&
-git checkout master $1 &&
+git checkout master dockerfiles/$1 &&
 git rm Dockerfile &&
-git mv $1 Dockerfile &&
+git mv dockerfiles/$1 Dockerfile &&
 git commit -m "Update Dockerfile for $1" &&
 git push origin $1
