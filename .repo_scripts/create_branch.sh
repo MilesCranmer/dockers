536929bab4ba9feae6fb4f8181ec559e19907817
MilesCranmer <miles.cranmer@gmail.com>
2017-06-02 14:10:01 +1000

    Add command to go back to master

--- .repo_scripts/create_branch.sh
+++ .repo_scripts/create_branch.sh
@@ -5,4 +5,5 @@ git checkout master dockerfiles/$1 &&
 git mv dockerfiles/$1 Dockerfile &&
 git add Dockerfile  &&
 git commit -m "Created branch for $1"  &&
-git push --set-upstream origin $1
+git push --set-upstream origin $1 &&
+git checkout master
--- .repo_scripts/update_branch.sh
+++ .repo_scripts/update_branch.sh
@@ -3,4 +3,5 @@ git checkout master dockerfiles/$1 &&
 git rm Dockerfile &&
 git mv dockerfiles/$1 Dockerfile &&
 git commit -m "Update Dockerfile for $1" &&
-git push origin $1
+git push origin $1 &&
+git checkout master
