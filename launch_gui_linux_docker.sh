a8600367d8c6d3622533899f0a746cd1ef35e55d
Ubuntu <ubuntu@ip-172-31-72-168.ec2.internal>
2017-07-21 15:22:40 +0000

    Modify linux docker command for AWS
    - Can run xeyes in AWS over ssh and inside a docker!

--- launch_gui_linux_docker.sh
+++ launch_gui_linux_docker.sh
@@ -3,7 +3,7 @@
 # Credit to: http://stackoverflow.com/a/36190462/2689923
 
 # Define what we are running here.
-CONTAINER=$1
+IMAGE=$1
 COMMAND=/bin/bash
 
 XSOCK=/tmp/.X11-unix
@@ -14,19 +14,22 @@ xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 # This sets the port access to 8888
 # The file storage (e.g., accessible from jupyter) to Documents/notebooks
 # This also sets up the display (for e.g., popouts if they happen (?))
-docker run \
+nvidia-docker run \
     -it \
+    --privileged \
     --rm \
     --workdir="/workspace" \
     -p 8888:8888 \
-    -v $2:/workspace \
+	 -v $HOME:/workspace \
+    --net=host \
+	 -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /usr/bin/docker:/usr/bin/docker \
+	 --security-opt seccomp=unconfined \
     -v $XSOCK:$XSOCK \
     -v $XAUTH:$XAUTH \
     -e XAUTHORITY=$XAUTH \
-    -e DISPLAY=unix$DISPLAY \
-    -e uid=$(id -u) \
-    -e gid=$(id -g) \
-    $CONTAINER \
-    $COMMAND
+    -e DISPLAY=$DISPLAY \
+    $IMAGE
+
 
 rm -f $XAUTH
